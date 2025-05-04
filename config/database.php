80511e4db01bc7ee302699555ec20f6bb85e51fa
Shameem Hussain Siam <115357770+altmiko@users.noreply.github.com>
2025-05-04 13:13:01 +0600

    Update database.php
    ;

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ $servername = "barberbookdb-barberbook.j.aivencloud.com";
 $username = "avnadmin";
 $password = "AVNS_EHS3bUWR3_7dcdFu9Ow";
 $dbname = "barberbookdb";
-$port = "14282"
+$port = "14282";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $port, $dbname);
